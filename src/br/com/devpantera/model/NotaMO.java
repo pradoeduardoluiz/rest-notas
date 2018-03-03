b24c1320b8cf399546642f59a4dffb28c0f3f371
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2018-03-03 19:06:13 -0300

    Primeiro commit do projeto de bloco de notas utilizando restful[C

---
+++ src/br/com/devpantera/model/NotaMO.java
@@ -0,0 +1,33 @@
+package br.com.devpantera.model;
+
+public class NotaMO {
+
+	private Integer id;
+	private String titulo;
+	private String descricao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
